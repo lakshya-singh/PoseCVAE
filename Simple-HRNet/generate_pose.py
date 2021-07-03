9a4c7a037da82cb78cd91da7caaf2712a2e4f698
Lakshya Kumar Singh <58124753+lakshya-singh@users.noreply.github.com>
2021-07-03 16:33:13 +0530

    Update generate_pose.py

--- Simple-HRNet/generate_pose.py
+++ Simple-HRNet/generate_pose.py
@@ -14,8 +14,8 @@ out_path = str(sys.argv[2])
 
 for section in sorted(os.listdir(path),key=numericalSort):
 	video_path=os.path.join(path,section)
-	pose_path=os.path.join(out_path,section)
-	pose_path = pose_path.split('.')[0] + '/'
+	pose_path=os.path.join(out_path,section.split('.')[0])
+	pose_path = pose_path + '/'
 	# print("\n")
 	# print(video_path)
 	# print(pose_path)
